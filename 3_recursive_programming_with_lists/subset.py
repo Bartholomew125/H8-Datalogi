865b961bbd5a9e64d46e5eb0bf860742e9f1bf96
Andreas Kapp Lindquist <126058312+Bartholomew125@users.noreply.github.com>
2024-09-25 14:37:12 +0200

    Update subset.py
    Fixed it. Den ville ikke virke for lister med forskellige lænder. Og tilføjede doc_tests

--- 3_recursive_programming_with_lists/subset.py
+++ 3_recursive_programming_with_lists/subset.py
@@ -1,6 +1,27 @@
-def subset(v: list, w: list) -> bool:
+def subset(v: list,w: list) -> bool:
+    """ Determines wether v is a subset of w, provided that v != []
+    >>> subset([1,2],[1,2,3])
+    True
+    >>> subset([1,2,3],[2,3,4])
+    False
+    """
     if v == []:
-         return True
-    elif w != [] and v[0] == w[0]:
-        return True and subset(v[1:], w[1:])
-    return False
+        return True
+    elif member(v[0],w):
+        return subset(v[1:],w)
+    else:
+        return False
+
+def member(x: any,v: list) -> bool:
+    """ Determines wether x is in v
+    >>> member(1,[1,2,3])
+    True
+    >>> member(0,[3,4,5])
+    False
+    """
+    if v == []:
+        return False
+    elif v[0] == x:
+        return True
+    else:
+        return member(x,v[1:])
