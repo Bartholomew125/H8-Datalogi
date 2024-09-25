d6ac7e04af9fd87b2ed77e998167d2d43a854544
Mikkel <mithe24@student.sdu.dk>
2024-09-25 11:53:56 +0200

    small changes

--- 2_recursive_programming_with_numbers/17_is_palindrome.py
+++ 2_recursive_programming_with_numbers/17_is_palindrome.py
@@ -5,21 +5,19 @@ def is_palindrome(n: int) -> bool:
     >>> is_palindrome(123)
     False
     """
-    L = _is_palindrome(n) # split n digits to list
-    if L == L[::-1]: # checks if palindrome
+    num = _is_palindrome(n) # split n digits to list
+    if num == num[::-1]: # checks if palindrome
         return True
-    else:
-        return False
+    return False
 
-def _is_palindrome(n: int, L: list = None) -> list:
+def _is_palindrome(n: int, num: list = None) -> list:
     """
     Helper function for is_palindrome.
     recursively appends each digit of n, to list.
     """
-    if L == None: # Creates empty list, for first recursion
-        L = []
-    if n == 0:
-        return L
-    else:
-        L.append(n % 10) # Appends last digit to list
-        return _is_palindrome(n // 10, L) # Recursion call
+    if num == None: # Creates empty list, for first recursion
+        num = []
+    if n == 0: # Halts recursion, when all digits in n, have been appended to list
+        return num
+    num.append(n % 10) # Appends last digit to list
+    return _is_palindrome(n // 10, num) # Recursion call
