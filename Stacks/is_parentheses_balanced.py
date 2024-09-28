f0d0f5203bdcec173a7358b3c54cf195c97533d9
Mikkel <mithe24@student.sdu.dk>
2024-09-28 19:13:06 +0200

    Stack ex

---
+++ Stacks/is_parentheses_balanced.py
@@ -0,0 +1,36 @@
+def is_parentheses_balanced(expression: str) -> bool:
+    """
+    Checks if parentheses are balanced.
+    >>> is_parentheses_balanced("{[()]}")
+    True
+    >>> is_parentheses_balanced("{[()}]")
+    False
+    >>> is_parentheses_balanced("(()")
+    False
+    """
+    # Defining parentheses and stack
+    stack = []
+    parentheses = {
+        '}': '{',
+        ')': '(',
+        ']': '[',
+    }
+
+    # Goes expression and appends open parentheses, and checks if closing parentheses match last parentheses
+    for char in expression:
+        # If open parentheses, append it to stack
+        if char in parentheses.values():
+            stack.append(char)
+        
+        # if closing parentheses match last parentheses, remove last element of stack
+        # else return False
+        elif char in parentheses.keys():
+            # Check closing parentheses match last parentheses
+            if not stack or stack[-1] != parentheses[char]:
+                return False
+            
+            # Removing last element of stack
+            stack.pop()
+
+    # Return True if stack is empty
+    return not stack
